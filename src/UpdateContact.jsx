f26563ba6c303bd27582cc74ff7dd7e22b21ff75
NickThelenProfessional <nickthelenprofessional@gmail.com>
2025-11-24 20:25:53 -0600

    Update UpdateContact.jsx

--- src/UpdateContact.jsx
+++ src/UpdateContact.jsx
@@ -17,7 +17,11 @@ export default function UpdateContact() {
     }
 
     try {
-      const res = await fetch(`http://localhost:8081/contacts/${searchName}`);
+      const encodedName = encodeURIComponent(searchName);
+
+      const res = await fetch(
+        `https://cs3870-backend-3kj9.onrender.com/contacts/${encodedName}`
+      );
       const data = await res.json().catch(() => null);
 
       if (res.status === 200) {
@@ -37,7 +41,8 @@ export default function UpdateContact() {
     setResponseMsg("");
 
     try {
-      const res = await fetch(`http://localhost:8081/contacts/${searchName}`, {
+      const encodedName = encodeURIComponent(searchName);
+      const res = await fetch(`https://cs3870-backend-3kj9.onrender.com/contacts/${encodedName}`, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
